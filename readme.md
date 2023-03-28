ca817e3f5e82594b45f827a02f610b051bac2caf
Michael <michael.d.nath@gmail.com>
2023-03-28 00:49:02 -0700

    Update readme

--- readme.md
+++ readme.md
@@ -1,2 +1,7 @@
-### MONTE CARLO TREE SEARCH
+# A Monte Carlo Tree Search (MCTS) Implementation
 
+## By Michael D. Nath
+
+> One thing that should be learned from the bitter lesson is the great power of general purpose methods, of methods that continue to scale with increased computation even as the available computation becomes very great. The two methods that seem to scale arbitrarily in this way are **search** and **learning**.
+>
+> — Richard Sutton in *A Bitter Lesson*
--- tests.py
+++ tests.py
@@ -1,10 +1,13 @@
+import numpy as np
 from NaiveMCTS import NaiveMCTS
 from games.tictactoe.TicTacToe import TicTacToeBoard
 from games.Player import Player
 from policies.RandomPolicy import RandomTTTPolicy
+import matplotlib.pyplot as plt
 
-NUM_ROWS = 3
-NUM_COLS = 3
+MCTS_INDICATOR = 1
+OPP_INDICATOR = 0
+N_TRIALS = 10
 
 def simulate(
     manual_play=False, 
@@ -12,11 +15,12 @@ def simulate(
     opponent_indicator=0,
     n_tree_iters=10000,
     verbose=False,
-    exploration_constant=1
+    exploration_constant=1/np.sqrt(2)
     ):
 
     tictactoe_game = TicTacToeBoard()
     mcts_brain = NaiveMCTS(tictactoe_game, mcts_indicator, opponent_indicator, RandomTTTPolicy(), exploration_constant=exploration_constant)
+    bot_policy = RandomTTTPolicy()
     bot_player = Player(TicTacToeBoard.indicator_to_mark(opponent_indicator))
     mcts_player = Player(TicTacToeBoard.indicator_to_mark(mcts_indicator))
 
@@ -25,7 +29,7 @@ def simulate(
             bot_action = input("Provide row, column\n").split(',')
             bot_action = [int(x) for x in bot_action]
         else: 
-            bot_action = policy.select_action(tictactoe_game.get_current_game_state())
+            bot_action = bot_policy.select_action(tictactoe_game.get_current_game_state())
         tictactoe_game.mark_move(bot_player, int(bot_action[0]), int(bot_action[1]))
         if verbose:
             print(f"Opponent is marking {bot_player.mark} at coordinate {bot_action}")
@@ -47,8 +51,12 @@ def simulate(
 
     _, winner = tictactoe_game.is_terminal_state(tictactoe_game)
     return winner
-
-def run_experiments(n_trials=100, verbose=False): 
+def run_experiments(
+    n_trials=N_TRIALS, 
+    verbose=False, 
+    exploration_constant=1, 
+    n_tree_iters=10000
+    ): 
     n_mcts_wins = 0
     n_opponent_wins = 0
     n_draws = 0
@@ -57,9 +65,9 @@ def run_experiments(n_trials=100, verbose=False):
             manual_play=False,
             mcts_indicator=1,
             opponent_indicator=0,
-            n_tree_iters=10000,            
+            n_tree_iters=n_tree_iters,            
             verbose=verbose,
-            exploration_constant=1.5
+            exploration_constant=exploration_constant
         )
         if winner == 1:
             n_mcts_wins += 1
@@ -67,8 +75,19 @@ def run_experiments(n_trials=100, verbose=False):
             n_opponent_wins += 1
         else:
             n_draws += 1
-    print(f"NUM MCTS WINS: {n_mcts_wins}/{n_trials} = {n_mcts_wins * 100 / n_trials}%") 
-    print(f"NUM OPPONENT WINS: {n_opponent_wins}/{n_trials} = {n_opponent_wins * 100 / n_trials}%")
-    print(f"NUM DRAWS: {n_draws}/{n_trials} = {n_draws * 100 / n_trials}%")
+    return n_mcts_wins
 
-run_experiments(n_trials=10, verbose=False)
+
+
+def vary_num_tree_iterations(n_max_tree_iters):
+    # This determines how granular we want our findings of optimal tree iterations to be.
+    n_checkpoints = 10
+    n_iterations_set = np.linspace(1, n_max_tree_iters, n_checkpoints, dtype=int)
+    mcts_wins = np.array([])
+    for n_tree_iters in n_iterations_set:
+        mcts_wins = np.append(mcts_wins, run_experiments(n_tree_iters=n_tree_iters))
+    plt.plot(n_iterations_set, mcts_wins / 100)
+    plt.savefig("plots/n_tree_iters.png")
+
+if __name__ == "__main__":
+    vary_num_tree_iterations(10000)
